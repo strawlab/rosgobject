594c641b38b761c31b8d6abe03fa2f32e5a1970d
John Stowers <john.stowers@gmail.com>
2013-01-24 12:04:57 +0100

    improve test gui

--- tests/testgui.py
+++ tests/testgui.py
@@ -5,6 +5,8 @@ import roslib; roslib.load_manifest('rosgobject')
 import argparse
 
 import rospy
+import std_msgs.msg
+
 import rosgobject.managers
 import rosgobject.gtk
 
@@ -13,17 +15,58 @@ from gi.repository import Gtk, GObject, Gdk
 GObject.threads_init()
 Gdk.threads_init()
 
+class CompositeTopicServiceWidget:
+    def __init__(self, basepath):
+        pass
+
 class UI:
     def __init__(self):
         self._w = Gtk.Window()
         self._w.set_default_size(600,400)
-        self._w.connect("delete-event", rosgobject.main_quit)
-        self._grid = Gtk.Grid(orientation=Gtk.Orientation.HORIZONTAL)
+        self._w.connect("delete-event", Gtk.main_quit)
+        self._grid = Gtk.Grid()
         self._w.add(self._grid)
 
+        self._add_topic_widgets()
         self._add_treeviews()
 
         self._w.show_all()
+
+    def _add_topic_widgets(self):
+        w = rosgobject.gtk.GtkEntryTopicDisplay(
+                "/testnode/string",
+                std_msgs.msg.String)
+        self._grid.insert_row(0)
+        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
+        self._grid.attach(w,1,0,1,1)
+
+        w = rosgobject.gtk.GtkEntryTopicDisplay(
+                "/testnode/float",
+                std_msgs.msg.Float32,
+                format_func=lambda x:"{0:5.1f} fps".format(x.data),
+                name="Framerate")
+        self._grid.insert_row(0)
+        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
+        self._grid.attach(w,1,0,1,1)
+
+        w = rosgobject.gtk.GtkComboBoxTextPublisher(
+                "~pub_string",
+                std_msgs.msg.String,
+                ("foo","bar","baz"),
+                name="Publish String")
+        self._grid.insert_row(0)
+        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
+        self._grid.attach(w,1,0,1,1)
+
+        w = rosgobject.gtk.GtkComboBoxTextPublisher(
+                "~pub_float",
+                std_msgs.msg.Float32,
+                ("-1","2.3","1234"),
+                type_conv_func=float,
+                name="Publish Float")
+        self._grid.insert_row(0)
+        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
+        self._grid.attach(w,1,0,1,1)
 
     def _add_treeviews(self):
         manager = rosgobject.managers.ROSNodeManager()
@@ -34,8 +77,8 @@ class UI:
                 Gtk.TreeViewColumn("Path", Gtk.CellRendererText(),
                 text=model.COL_ID_PATH))
         tree.props.hexpand = True
-        tree.props.vexpand = True
-        self._grid.add(tree)
+        self._grid.insert_row(0)
+        self._grid.attach(tree,0,0,2,1)
 
         model = rosgobject.gtk.ROSNodeTreeModel(manager)
         tree = Gtk.TreeView(model)
@@ -46,11 +89,12 @@ class UI:
                 Gtk.TreeViewColumn("Type", Gtk.CellRendererText(),
                 text=model.COL_ID_TYPE))
         tree.props.hexpand = True
-        tree.props.vexpand = True
-        self._grid.add(tree)
+        self._grid.insert_row(0)
+        self._grid.attach(tree,0,0,2,1)
 
 if __name__ == "__main__":
     rospy.init_node("testgui", anonymous=True)
     ros = rosgobject.get_ros_thread() #ensure ros is spinning
     u = UI()
     Gtk.main()
+
