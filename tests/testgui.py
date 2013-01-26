faa9aa71faa941a5bfa281ce242a753cd419cc71
John Stowers <john.stowers@gmail.com>
2013-01-26 21:25:08 +0100

    tests: update to new API

--- tests/testgui.py
+++ tests/testgui.py
@@ -1,100 +1,130 @@
-#!/usr/bin/env python
 # -*- Mode: python; tab-width: 4; indent-tabs-mode: nil; indent-offset: 4 -*-
 import roslib; roslib.load_manifest('rosgobject')
 
 import argparse
+import logging
 
 import rospy
 import std_msgs.msg
+import std_srvs.srv
 
 import rosgobject.managers
 import rosgobject.gtk
+from rosgobject.wrappers import *
 
-from gi.repository import Gtk, GObject, Gdk
-
-GObject.threads_init()
-Gdk.threads_init()
-
-class CompositeTopicServiceWidget:
-    def __init__(self, basepath):
-        pass
+from gi.repository import Gtk
 
 class UI:
     def __init__(self):
         self._w = Gtk.Window()
-        self._w.set_default_size(600,400)
-        self._w.connect("delete-event", Gtk.main_quit)
+        self._w.connect("delete-event", rosgobject.main_quit)
         self._grid = Gtk.Grid()
+        self._widgets = []
+
+        self._manager = rosgobject.managers.ROSNodeManager()
+        self._build_ui()
+
         self._w.add(self._grid)
-
-        self._add_topic_widgets()
-        self._add_treeviews()
-
         self._w.show_all()
 
-    def _add_topic_widgets(self):
-        w = rosgobject.gtk.GtkEntryTopicDisplay(
-                "/testnode/string",
-                std_msgs.msg.String)
+    def _add_widget(self, w):
+        self._widgets.append(w)
         self._grid.insert_row(0)
-        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
-        self._grid.attach(w,1,0,1,1)
+        self._grid.attach(w.label,0,0,1,1)
+        self._grid.attach(w.widget,1,0,1,1)
 
-        w = rosgobject.gtk.GtkEntryTopicDisplay(
-                "/testnode/float",
-                std_msgs.msg.Float32,
+    def _build_ui(self):
+        w = GtkEntryTopicWidget(
+                nodepath="/testnode/string",
+                msgclass=std_msgs.msg.String)
+        self._add_widget(w)
+
+        w = GtkEntryTopicWidget(
+                nodepath="/testnode/float",
+                msgclass=std_msgs.msg.Float32,
                 format_func=lambda x:"{0:5.1f} fps".format(x.data),
                 name="Framerate")
-        self._grid.insert_row(0)
-        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
-        self._grid.attach(w,1,0,1,1)
+        self._add_widget(w)
 
-        w = rosgobject.gtk.GtkComboBoxTextPublisher(
-                "~pub_string",
-                std_msgs.msg.String,
-                ("foo","bar","baz"),
-                name="Publish String")
-        self._grid.insert_row(0)
-        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
-        self._grid.attach(w,1,0,1,1)
+        w = GtkSwitchTopicWidget(
+                nodepath="/testnode/bool",
+                msgclass=std_msgs.msg.Bool,
+                name="Power")
+        self._add_widget(w)
 
-        w = rosgobject.gtk.GtkComboBoxTextPublisher(
-                "~pub_float",
-                std_msgs.msg.Float32,
-                ("-1","2.3","1234"),
-                type_conv_func=float,
-                name="Publish Float")
-        self._grid.insert_row(0)
-        self._grid.attach(Gtk.Label(w.name,xalign=0.0),0,0,1,1)
-        self._grid.attach(w,1,0,1,1)
+        w = GtkImageTopicWidget(
+                nodepath="/testnode/bool",
+                msgclass=std_msgs.msg.Bool)
+        self._add_widget(w)
 
-    def _add_treeviews(self):
-        manager = rosgobject.managers.ROSNodeManager()
+        w = GtkComboBoxTextPublisherWidget(
+                options=("foo","bar","baz"),
+                nodepath="~pub_string",
+                msgclass=std_msgs.msg.String)
+        self._add_widget(w)
 
-        model = rosgobject.gtk.ROSTopicListModel(manager, "std_msgs/Float32")
-        tree = Gtk.TreeView(model)
-        tree.append_column(
-                Gtk.TreeViewColumn("Path", Gtk.CellRendererText(),
-                text=model.COL_ID_PATH))
-        tree.props.hexpand = True
-        self._grid.insert_row(0)
-        self._grid.attach(tree,0,0,2,1)
+        w = GtkSpinButtonPublisherWidget(
+                min=10,
+                max=100,
+                step=0.1,
+                nodepath="/some_float",
+                msgclass=std_msgs.msg.Float32,
+                name="Publish a Float")
+        self._add_widget(w)
 
-        model = rosgobject.gtk.ROSNodeTreeModel(manager)
-        tree = Gtk.TreeView(model)
-        tree.append_column(
-                Gtk.TreeViewColumn("Path", Gtk.CellRendererText(),
-                text=model.COL_ID_PATH))
-        tree.append_column(
-                Gtk.TreeViewColumn("Type", Gtk.CellRendererText(),
-                text=model.COL_ID_TYPE))
-        tree.props.hexpand = True
-        self._grid.insert_row(0)
-        self._grid.attach(tree,0,0,2,1)
+        w = GtkButtonPublisherWidget(
+                nodepath="/some_uint",
+                msgclass=std_msgs.msg.UInt32,
+                name="Click to Publish")
+        self._add_widget(w)
+
+        w = GtkButtonPublisherWidget(
+                widget=Gtk.ToggleButton("Toggle!"),
+                nodepath="/some_bool",
+                msgclass=std_msgs.msg.Bool)
+        self._add_widget(w)
+
+        w = GtkEntryViewParam(
+                nodepath="/param_a")
+        self._add_widget(w)
+        w = GtkEntryViewParam(
+                nodepath="/param_b")
+        self._add_widget(w)
+        w = GtkSpinButtonParam(
+                nodepath="/param_b",
+                min=0.5,
+                max=5,
+                step=0.01)
+        self._add_widget(w)
+
+        w = GtkButtonKillNode(
+                nodepath="/newtestnode",
+                nodemanager=self._manager,
+                name="Kill /newtestnode")
+        self._add_widget(w)
+
+        w = GtkButtonStartNode(
+                nodepath="newtestnode",
+                nodemanager=self._manager,
+                package="rosgobject",
+                node_type="testnode.py",
+                name="Start /newtestnode")
+        self._add_widget(w)
+
+        w = GtkButtonSetServiceWidget(
+                nodepath="/testnode/empty_service",
+                srvclass=std_srvs.srv.Empty,
+                name="Call /testnode/empty_service")
+        self._add_widget(w)
+        w = GtkEntryTopicWidget(
+                nodepath="/testnode/change_on_service_call",
+                msgclass=std_msgs.msg.Float32)
+        self._add_widget(w)
 
 if __name__ == "__main__":
     rospy.init_node("testgui", anonymous=True)
-    ros = rosgobject.get_ros_thread() #ensure ros is spinning
+    rosgobject.get_ros_thread() #ensure ros is spinning
+    rosgobject.add_console_logger()
     u = UI()
     Gtk.main()
 
